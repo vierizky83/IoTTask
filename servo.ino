bbf670cdbc468debfcfbb6aaa8e59eb6728b3f5b
vierizky <90898630+vierizky83@users.noreply.github.com>
2022-06-16 09:45:45 +0700

    Add files via upload

---
+++ servo.ino
@@ -0,0 +1,21 @@
+#include <M5Stack.h>
+#include "driver/ledc.h"
+const int servo_pin = 15;
+int freq = 50;
+int ledChannel = 0;
+int resolution = 8;
+void setup() {
+M5.begin();
+M5.Lcd.setCursor(120, 110, 4);
+M5.Lcd.println("SERVO");
+ledcSetup(ledChannel, freq, resolution);
+ledcAttachPin(servo_pin, ledChannel);
+}
+void loop() {
+ledcWrite(ledChannel, 6);//0°
+delay(1000);
+ledcWrite(ledChannel, 18);//90°
+delay(1000);
+ledcWrite(ledChannel, 30);//180°
+delay(1000);
+}
